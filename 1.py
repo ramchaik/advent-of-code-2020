1610fc43a42f1463d7c77874624bca28642fb245
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-07-24 18:25:33 +0530

    perf: optimise sol 1 to O(n)

--- 1.py
+++ 1.py
@@ -1,6 +1,9 @@
 def find_answer(input):
+  sum_map = {}
   for x in input:
     next_sum = 2020 - x
-    if next_sum in input:
+    if next_sum in sum_map:
       return next_sum * x
+    else:
+      sum_map[x] = True
   return "You are messing with me!" 
