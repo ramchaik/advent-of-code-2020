833e2cc0a1fe26f81ff2f33554483636138dd3d0
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-07-27 01:16:56 +0530

    feat: add part 2 of q2

--- 2.py
+++ 2.py
@@ -1,7 +1,7 @@
 def find_answer(input):
   ans = 0
   for i in input:
-    [policy, password] = i.split(':')
+    [policy, password] = i.split(': ')
     [min_max_len, char] = policy.split() 
     [min_limit, max_limit] = min_max_len.split('-')
     if char not in password:
@@ -9,4 +9,21 @@ def find_answer(input):
     current_char_count = password.count(char)
     if int(min_limit) <= current_char_count and int(max_limit) >= current_char_count:
       ans += 1
+  return ans
+
+def find_answer_2(input):
+  ans = 0
+  for i in input:
+    [policy, password] = i.split(': ')
+    [indxes, char] = policy.split() 
+    [idx_1, idx_2] = indxes.split('-')
+
+    if char not in password:
+      continue
+
+    if (password[int(idx_1) - 1] == char) or (password[int(idx_2) - 1] == char):
+      if password[int(idx_1) - 1] == char and password[int(idx_2) - 1] == char:
+        continue
+      else: 
+        ans += 1
   return ans
