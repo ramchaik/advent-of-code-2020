05ec8bb969488ec11a225626295fd8f4ac16f4a1
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-07-27 01:34:55 +0530

    feat: add part 2 of q1

--- 1.py
+++ 1.py
@@ -1,4 +1,5 @@
 def find_answer(input):
+  # two sum
   sum_map = {}
   for x in input:
     next_sum = 2020 - x
@@ -6,4 +7,17 @@ def find_answer(input):
       return next_sum * x
     else:
       sum_map[x] = True
+  return "You are messing with me!" 
+
+def find_answer_2(input):
+  # three sum, could avoid extra steps in loop but meh!
+  for x in input:
+    next_sum = 2020 - x
+    sum_map = {}
+    for y in input:
+      next_next_sum = next_sum - y 
+      if next_next_sum in sum_map:
+        return next_next_sum * x * y
+      else:
+        sum_map[y] = True
   return "You are messing with me!" 
