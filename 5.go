c6265305bf1dae0e5c8396973c9844668a67f077
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-08-21 23:55:06 +0530

    feat: part 1 q 5

---
+++ 5.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func readInput(path string) ([]string, error) {
+	file, err := os.Open(path)
+
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines, scanner.Err()
+}
+
+func bsp(lower int,upper int, l string, u string) func(string, int, int) int {
+	return  func(str string, sStart int, sEnd int) int {
+		mid := 0
+
+		for _,row := range strings.Split(str, "")[sStart: sEnd] {
+			mid = (upper + lower) / 2
+
+			if (row == l) {
+				upper = mid
+			}
+
+			if (row == u) {
+				lower = mid + 1
+			}
+		}
+		return lower
+	}
+}
+
+func getRow(boardingPass string) int {
+	return bsp(0, 127, "F", "B")(boardingPass, 0, 7)
+}
+
+func getColumn(boardingPass string) int {
+	return bsp(0, 7, "L", "R")(boardingPass, 7, 10)
+}
+
+func getSeatId(row int, column int) int {
+	return row * 8 + column
+}
+
+func getMaxSeatId(seatIds []int) int {
+	var maxSeatId int = 0
+	for i, s := range seatIds {
+    if i==0 || s > maxSeatId {
+      maxSeatId = s
+    }
+	}
+	return maxSeatId
+}
+
+func main() {
+	sample, e  := readInput("input.txt")
+	if e != nil {
+		fmt.Println("Reading input", e)
+	}
+
+	var seatIds []int
+
+	for _, boardingPass := range sample {
+		row := getRow(boardingPass)
+		column := getColumn(boardingPass)
+		seatId := getSeatId(row, column)
+		seatIds = append(seatIds, seatId)
+	} 
+
+	maxSeatId := getMaxSeatId(seatIds)
+	fmt.Println("ans :>> ", maxSeatId)
+}
